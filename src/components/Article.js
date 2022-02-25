845cbaa846a262d857431076ae222eb72a3b0ef3
Taylor Huffman <taylor@crucialartists.com>
2022-02-25 12:31:54 -0600

    Complete bonus feature

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,32 @@
 import React from "react";
 
-function Article({ title, date = "January 1, 1970", preview }) {
-  return (
-    <article>
-        <h3>{title}</h3>
-        <small>{date}</small>
-        <p>{preview}</p>
-    </article>
-  );
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+    const coffeeCup = "☕️"
+    const bentoBox = "🍱"
+
+    function round5(x) {
+        return Math.ceil(x/5)*5;
+    }
+
+    function round10(x) {
+        return Math.ceil(x/10)*10;
+    }
+
+    function displayEmoji() {
+        if (minutes < 30) {
+            return coffeeCup.repeat(round5(minutes)/5)
+        } else {
+            return bentoBox.repeat(round10(minutes)/10)
+        }
+    }
+
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date} &#8226; {displayEmoji()} {minutes + " min read"}</small>
+            <p>{preview}</p>
+        </article>
+    );
 }
 
 export default Article;
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,7 +3,7 @@ import Article from "./Article";
 
 function ArticleList({ posts }) {
     const articleItem = posts.map((articleObj) => {
-        return <Article key={articleObj.id} title={articleObj.title} date={articleObj.date} preview={articleObj.preview} />
+        return <Article key={articleObj.id} title={articleObj.title} date={articleObj.date} preview={articleObj.preview} minutes={articleObj.minutes} />
     })
   return (
     <main>
