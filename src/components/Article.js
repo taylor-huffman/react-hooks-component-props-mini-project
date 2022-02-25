90b55eb0bf6c695ed0de033048cec26651bdab7a
Taylor Huffman <taylor@crucialartists.com>
2022-02-25 13:14:45 -0600

    Refactor round function

--- src/components/Article.js
+++ src/components/Article.js
@@ -4,19 +4,15 @@ function Article({ title, date = "January 1, 1970", preview, minutes }) {
     const coffeeCup = "☕️"
     const bentoBox = "🍱"
 
-    function round5(x) {
-        return Math.ceil(x/5)*5;
-    }
-
-    function round10(x) {
-        return Math.ceil(x/10)*10;
+    function round(min, value) {
+        return (Math.ceil(min/value)*value)/value;
     }
 
     function displayEmoji() {
         if (minutes < 30) {
-            return coffeeCup.repeat(round5(minutes)/5)
+            return coffeeCup.repeat(round(minutes, 5))
         } else {
-            return bentoBox.repeat(round10(minutes)/10)
+            return bentoBox.repeat(round(minutes, 10))
         }
     }
 
