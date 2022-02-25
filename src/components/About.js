5db4430e2357c64f430cd5a4e78682713456d4c2
Taylor Huffman <taylor@crucialartists.com>
2022-02-25 11:46:48 -0600

    Complete main deliverables

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 function About({ image = "https://via.placeholder.com/215", about }) {
   return (
     <aside>
-        <img src={image}></img>
+        <img src={image} alt="blog logo"></img>
         <p>{about}</p>
     </aside>
   );
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Article from "./Article";
 
-function ArticleList() {
+function ArticleList({ posts }) {
+    const articleItem = posts.map((articleObj) => {
+        return <Article key={articleObj.id} title={articleObj.title} date={articleObj.date} preview={articleObj.preview} />
+    })
   return (
-    <div>
-      
-    </div>
+    <main>
+        {articleItem}
+    </main>
   );
 }
 
